7279b9f7a1822e8563114c0dc011d1818ccdd588
Mehdi <mehdi.rochereau@hotmail.fr>
2024-09-09 17:08:11 +0200

    fonctionnel 09/09/2024

--- src/main/java/fr/eni/pizza/PizzaApplication.java
+++ src/main/java/fr/eni/pizza/PizzaApplication.java
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoGitApplication {
-
-
-
+public class PizzaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoGitApplication.class, args);
+		SpringApplication.run(PizzaApplication.class, args);
 	}
 
 }
