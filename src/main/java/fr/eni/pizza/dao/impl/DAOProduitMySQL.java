0841d330986b611f73ccd2f326ded34205c87e64
Mehdi <mehdi.rochereau@hotmail.fr>
2024-09-10 14:28:02 +0200

    produit sql

--- src/main/java/fr/eni/pizza/dao/impl/DAOProduitMySQL.java
+++ src/main/java/fr/eni/pizza/dao/impl/DAOProduitMySQL.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
@@ -19,6 +21,9 @@ public class DAOProduitMySQL implements IDAOProduit {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     //Le code qui permet de savoir comment convertir/mapper un résultat en SQL en objet Film
     //Comment mapper un résultat SQL en Film
@@ -38,7 +43,7 @@ public class DAOProduitMySQL implements IDAOProduit {
             //On instancie un Type_Produit à mapper
             TypeProduit typeProduit = new TypeProduit();
             //On détermine les colonnes de la table Type_Produit qui seront mapper
-            //typeProduit.setId_type_produit(rs.getLong("id_type_produit"));
+            typeProduit.setId_type_produit(rs.getLong("TYPE_PRODUIT_id_type_produit"));
             typeProduit.setLibelle(rs.getString("libelle"));
 
             //Maintenant on peut mapper la colonne "TYPE_PRODUIT_id_type_produit" de Produit
@@ -49,34 +54,47 @@ public class DAOProduitMySQL implements IDAOProduit {
         }
     };
 
+   private MapSqlParameterSource map (Produit produit) {
+
+       MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+       mapSqlParameterSource.addValue("idProduit", produit.getId_produit());
+       mapSqlParameterSource.addValue("nomProduit", produit.getNom());
+       mapSqlParameterSource.addValue("descriptionProduit", produit.getDescription());
+       mapSqlParameterSource.addValue("prixProduit", produit.getPrix());
+       mapSqlParameterSource.addValue("imageProduit", produit.getImage_url());
+       mapSqlParameterSource.addValue("typeProduit", produit.getId_type_produit());
+
+       return mapSqlParameterSource;
+    }
+
     private String sqlSelectAllProduits = "SELECT p.id_produit, p.nom, p.description, p.prix, p.image_url, t.id_type_produit AS TYPE_PRODUIT_id_type_produit, t.libelle FROM produit p JOIN type_produit t ON p.TYPE_PRODUIT_id_type_produit = t.id_type_produit";
     private String sqlSelectProduitById = "SELECT p.id_produit, p.nom, p.description, p.prix, p.image_url, t.id_type_produit AS TYPE_PRODUIT_id_type_produit, t.libelle FROM produit p JOIN type_produit t ON p.TYPE_PRODUIT_id_type_produit = t.id_type_produit WHERE p.id_produit = ?";
+    private String sqlInsertProduit = "INSERT INTO produit(id_produit, nom, description, prix, image_url, TYPE_PRODUIT_id_type_produit) VALUES (:idProduit, :nomProduit, :descriptionProduit, :prixProduit, :imageProduit, :typeProduit)";
+    private String sqlUpdateProduit = "UPDATE produit SET nom = :nomProduit, description = :descriptionProduit, prix = :prixProduit, image_url = :imageProduit, TYPE_PRODUIT_id_type_produit = :typeProduit WHERE id_produit = :idProduit";
+    private String sqlDeleteProduit = "DELETE FROM produit WHERE id_produit = :idProduit";
 
     @Override
     public List<Produit> findAllProduits() {
-
         return jdbcTemplate.query(sqlSelectAllProduits, PRODUIT_RAW_MAPPER);
     }
 
-
     @Override
     public Produit findProduitById(Long id_produit) {
-
         return jdbcTemplate.queryForObject(sqlSelectProduitById, PRODUIT_RAW_MAPPER, id_produit);
     }
 
     @Override
     public void addProduitToDB(Produit produit) {
-
+        namedParameterJdbcTemplate.update(sqlInsertProduit, map(produit));
     }
 
     @Override
     public void updateProduitToDB(Produit produit) {
-
+        namedParameterJdbcTemplate.update(sqlUpdateProduit, map(produit));
     }
 
     @Override
     public void deleteProduitToDB(Produit produit) {
-
+        namedParameterJdbcTemplate.update(sqlDeleteProduit, map(produit));
     }
 }
