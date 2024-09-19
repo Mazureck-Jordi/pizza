41dada508ed207ab067e9b23d4e295edef22c844
Mehdi <mehdi.rochereau@hotmail.fr>
2024-09-19 15:25:10 +0200

    19/09/2024

--- src/main/java/fr/eni/pizza/ihm/TestController.java
+++ src/main/java/fr/eni/pizza/ihm/TestController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import java.util.List;
 
 @SessionAttributes({"loggedUser"})
-@SessionAttributes("commande")
 @Controller
 public class TestController {
 
